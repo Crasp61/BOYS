3aa97fac283e175b0c49535ec8b6e0f4b59edd16
Crasp61 <kosmogames@yandex.ru>
2022-11-06 13:54:10 +0300

    DashMech

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -8,11 +8,11 @@ public abstract class Creature : MonoBehaviour
     [SerializeField] protected float _movementSpeed = 4f;
     [SerializeField] protected int _health = 30;
     [SerializeField] protected int _damage = 5;
-    protected Rigidbody2D rb;
+    [SerializeField] protected Rigidbody2D rb;
     
     protected virtual void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     protected abstract void Move();
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,43 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Player : Creature
 {
     [SerializeField] private float jumpForce = 4f;
     [SerializeField] private bool isGround = false;
-    [SerializeField] private float _dashForce;
-    private float _timer;
-    [SerializeField] private float _dashCoolDown;
+    private float _horizontal;
+    private bool _isFacingRight = true;
+    
+    private void Update()
+    {
+        if (_isDashing)
+        {
+            return;
+        }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-            isGround = true;
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-            isGround = false;
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _canDash)
+        {
+            StartCoroutine(Dash()); 
+        }
+
+        Flip();
     }
 
     private void FixedUpdate()
     {
-        Move();
-        Dash();
+        if (_isDashing)
+        {
+            return;
+        }
         jump();
+        FixedMove();
     }
 
-     protected override void Move()
+    protected override void Move()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(transform.right * _movementSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-transform.right * _movementSpeed * Time.deltaTime);
-        }
+        _horizontal = Input.GetAxisRaw("Horizontal");
     }
 
+    private void FixedMove()
+    {
+        rb.velocity = new Vector2(_horizontal * _movementSpeed, rb.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGround = false;
+    }
     private void jump()
     {
         if (Input.GetKey(KeyCode.Space) && isGround)
@@ -46,8 +62,39 @@ public class Player : Creature
         }
     }
 
-    protected void Dash()
+    private bool _canDash = true;
+    private bool _isDashing;
+    [SerializeField] private float _dashingPower = 24f;
+    [SerializeField] private float _dashingTime = 0.2f;
+    [SerializeField] private float _dashingCoolDown = 1f;
+    [SerializeField] private TrailRenderer tr;
+
+    private IEnumerator Dash()
     {
-        
+        _canDash = false;
+        _isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * _dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(_dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        _isDashing = false;
+        yield return new WaitForSeconds(_dashingCoolDown);
+        _canDash = true;
+    }
+
+    private void Flip()
+    {
+        if (_isFacingRight && _horizontal < 0f || !_isFacingRight && _horizontal > 0f)
+        {
+
+            Vector3 localScale = transform.localScale;
+            _isFacingRight = !_isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+
+        }
     }
 }
