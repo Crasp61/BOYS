4c25ce6d6c0a9b3aacf68d6aa12cbdb464afa10a
Scroga <ilia_riabko@mail.ru>
2022-11-19 17:01:45 +0100

    BackgroundSmoke

---
+++ Assets/Scripts/MoveBG.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBG : MonoBehaviour
+{
+    public float speed;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
